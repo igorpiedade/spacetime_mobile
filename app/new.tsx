7a726d253e2cad44974aa81dea8ce873e936521d
Igor Piedade <igorpiedade@hotmail.com>
2023-06-20 05:14:23 -0400

    Add new memory page form

--- app/new.tsx
+++ app/new.tsx
@@ -1,22 +1,87 @@
-import { View, Switch, Text } from 'react-native'
+/* eslint-disable react-hooks/rules-of-hooks */
+import {
+  View,
+  Switch,
+  Text,
+  Image,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native'
 import Icon from '@expo/vector-icons/Feather'
 
 import SpacetimeLogo from '../src/assets/spacetimeLogo.svg'
 import { styled } from 'nativewind'
-import { Link } from 'expo-router'
-import {
-  ScrollView,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native-gesture-handler'
+import { Link, useRouter } from 'expo-router'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useState } from 'react'
+import * as ImagePicker from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
+
 const StyledLogo = styled(SpacetimeLogo)
 
 export default function newMemory() {
   const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
 
+  const [preview, setPreview] = useState<string | null>(null)
+  const [content, setContent] = useState('')
   const [isPublic, setIsPublic] = useState(false)
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (err) {
+      console.log('User did not select a picture')
+    }
+  }
+
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverURL = ''
+
+    if (preview) {
+      const uploadFormData = new FormData()
+
+      uploadFormData.append('file', {
+        uri: preview,
+        name: 'image.jpg',
+        type: 'image/jpeg',
+      } as any)
+
+      const uploadResponse = await api.post('/upload', uploadFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-fileIrl',
+        },
+      })
+      coverURL = uploadResponse.data.fileUrl
+    }
+
+    await api.post(
+      '/memories',
+      {
+        content,
+        isPublic,
+        coverURL,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    router.push('/memories')
+  }
 
   return (
     <ScrollView
@@ -48,18 +113,28 @@ export default function newMemory() {
         </View>
         <TouchableOpacity
           activeOpacity={0.6}
+          onPress={openImagePicker}
           className=" h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
         >
-          <View className="flex-row items-center gap-2">
-            <Icon name="image" color={'#FFF'} />
-            <Text className="font-body text-sm text-gray-200">
-              Add Photo or Video
-            </Text>
-          </View>
+          {preview ? (
+            <Image
+              source={{ uri: preview }}
+              className="h-full w-full rounded-lg object-cover"
+            />
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color={'#FFF'} />
+              <Text className="font-body text-sm text-gray-200">
+                Add Photo or Video
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
 
         <TextInput
           multiline
+          value={content}
+          onChangeText={setContent}
           className="p-0 font-body text-lg text-gray-50"
           placeholderTextColor="#56565a"
           placeholder="Add a description of this moment to remember. Photos, Videos e how you feel at that moment is only some examples of what you can do here."
@@ -67,6 +142,7 @@ export default function newMemory() {
 
         <TouchableOpacity
           activeOpacity={0.6}
+          onPress={handleCreateMemory}
           className="mt-6 h-10 w-20 items-center self-end rounded-full bg-green-500 px-5 py-2"
         >
           <Text className="font-alt text-sm uppercase text-black"> SAVE</Text>
