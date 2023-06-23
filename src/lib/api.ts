5929cceb2803b79ccd66cd4c2de501b68838eefd
Igor Piedade <igorpiedade@hotmail.com>
2023-06-23 15:15:21 -0400

    fixed some bugs and updating backend URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   // while testing, change localhost to your backend IP
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://192.168.1.100:3333',
 })
